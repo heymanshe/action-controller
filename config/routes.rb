7543af15da509edd3d14908f612ce71b19c3631d
heymanshe <himanshiksharma@outlook.com>
2025-03-03 10:40:12 +0530

    hash and array parameters

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "users/index"
+  get "users/create"
   get "clients/new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -11,6 +13,8 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   resources :clients, only: [ :create, :new ]
+
+  resources :users, only: [ :index, :create ]
   # Defines the root path route ("/")
   # root "posts#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_28_073858) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_03_050032) do
   create_table "clients", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.text "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
