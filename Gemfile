35dd7baeca47d04f32712275187718e4d6faa924
heymanshe <himanshiksharma@outlook.com>
2025-03-04 17:23:57 +0530

    restful downloads

--- Gemfile
+++ Gemfile
@@ -17,6 +17,9 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 gem "prawn"
+gem "wicked_pdf"
+gem "wkhtmltopdf-binary"
+
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -7,6 +7,13 @@ class ClientsController < ApplicationController
 
   def show
     @client = Client.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "client", template: "clients/show"
+      end
+    end
   end
 
   def new
@@ -24,26 +31,28 @@ class ClientsController < ApplicationController
 
 
   def download_pdf
-    client = Client.find(params[:id])
+    # client = Client.find(params[:id])
     # file_path = Rails.root.join("files", "clients", "#{client.name}.pdf")
     # send_data generate_pdf(client),
     #           filename: "#{client.name}.pdf",
     #           type: "application/pdf",
     #           disposition: "attachment"
 
-    send_file("#{Rails.root}/files/clients/#{client.name}.pdf",
-          filename: "#{client.name}.pdf",
-          type: "application/pdf")
+    # send_file("#{Rails.root}/files/clients/#{client.name}.pdf",
+    #       filename: "#{client.name}.pdf",
+    #       type: "application/pdf")
   end
 
-  # private
+  private
 
-  # def generate_pdf(client)
-  #   Prawn::Document.new do
-  #     text client.name, align: :center, size: 20, style: :bold
-  #     move_down 20
-  #     text "Address: #{client.address}", size: 14
-  #     text "Email: #{client.email}", size: 14
-  #   end.render
-  # end
+  def generate_pdf(client)
+    #   Prawn::Document.new do
+    #     text client.name, align: :center, size: 20, style: :bold
+    #     move_down 20
+    #     text "Address: #{client.address}", size: 14
+    #     text "Email: #{client.email}", size: 14
+    #   end.render
+    # end
+    "PDF Content for #{client.name}"
+  end
 end
